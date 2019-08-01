bd6e88549017df677527d7fead7d4c288872360d
ArturPidzharenko <arturpidzharenko@gmail.com>
2019-08-01 15:46:23 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// JS
+import './js/'
+
+// SCSS
+import './assets/scss/main.scss'
+
+//fortawesome icons
+import '@fortawesome/fontawesome-free/js/all';
+
+// CSS (example)
+//import './assets/css/main.css'
+
+
+
